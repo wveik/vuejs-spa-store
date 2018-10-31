904cb913f66bec7a9a8970445c729ea16c631ef5
wveik <kataev_06@list.ru>
2018-10-31 16:42:48 +0300

    Подключение и настройка Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import store from './store'
+import * as fb from 'firebase/app'
 
 Vue.use(Vuetify)
 
@@ -15,5 +16,17 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+     // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyCq1RpjqvYpExABFlI-O2EuQQDi0UQxq4Y',
+      authDomain: 'onlinestore-a41f8.firebaseapp.com',
+      databaseURL: 'https://onlinestore-a41f8.firebaseio.com',
+      projectId: 'onlinestore-a41f8',
+      storageBucket: 'onlinestore-a41f8.appspot.com',
+      messagingSenderId: '639762892367'
+    }
+    fb.initializeApp(config)
+  }
 })
